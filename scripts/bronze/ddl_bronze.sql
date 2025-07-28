02e210980b4029689bf8300219c9e128ccad4ac5
Jaqueline Justino <117241043+jaquelinejustino@users.noreply.github.com>
2025-07-27 22:00:55 -0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -23,7 +23,7 @@ CREATE TABLE bronze.crm_cust_info (
 );
 GO
 	
----
+
 	
 IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_prd_info;
@@ -40,7 +40,7 @@ CREATE TABLE bronze.crm_prd_info (
 );
 GO
 
----
+
 	
 IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
@@ -59,7 +59,7 @@ CREATE TABLE bronze.crm_sales_details (
 );
 GO
 
----
+
 	
 IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
@@ -71,7 +71,7 @@ CREATE TABLE bronze.erp_loc_a101 (
 );
 GO
 
----
+
 	
 IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
@@ -84,7 +84,7 @@ CREATE TABLE bronze.erp_cust_az12 (
 );
 GO
 
----
+
 	
 IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
